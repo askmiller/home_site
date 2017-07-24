b426bbace5048c5c13bce33b75a3bb190c93365a
askmiller <ajmiller2@mail.widener.edu>
2017-07-23 21:26:46 -0400

    began using bootstrap and added in basic database for models.  Created an early look and feel.

--- neuralnet/models.py
+++ neuralnet/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Network(models.Model):
+    name = models.CharField(max_length=250)
+    description = models.CharField(max_length=1000)
+
+class Layer(models.Model):
+    type = models.CharField(max_length=250)
+    network = models.ForeignKey(Network, on_delete=models.CASCADE)
+
+class Node(models.Model):
+    layer = models.ForeignKey(Layer, on_delete=models.CASCADE)
+    activation = models.CharField(max_length=1000)
--- neuralnet/views.py
+++ neuralnet/views.py
@@ -4,4 +4,4 @@ from django.views.generic import TemplateView
 
 class HomeView(TemplateView):
     def get(selfself, request, **kwarts):
-        return render(request, 'neuralnet/index.html', context={"title": "test"})
+        return render(request, 'neuralnet/index.html', context={"title": "Neural Network"})
